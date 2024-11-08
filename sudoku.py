80175eedb32705cd79768af3a2c5f94fadc9f56c
adhi43 <86366682+adhi43@users.noreply.github.com>
2024-11-08 16:38:22 +0530

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -1,11 +1,10 @@
 import tkinter as tk
 from tkinter import messagebox
 
-
 def solve_sudoku(board):
     empty = find_empty(board)
     if not empty:
-        return True  # Puzzle solved
+        return True  # Puzzle solved      
     row, col = empty
 
     for num in range(1, 10):
@@ -19,14 +18,12 @@ def solve_sudoku(board):
 
     return False
 
-
 def find_empty(board):
     for i in range(9):
         for j in range(9):
             if board[i][j] == 0:
                 return (i, j)  # row, col
     return None
-
 
 def is_valid(board, num, pos):
     row, col = pos
@@ -49,16 +46,15 @@ def is_valid(board, num, pos):
 
     return True
 
-
 def check_for_duplicates(board):
     for i in range(9):
         row = [num for num in board[i] if num != 0]
         if len(row) != len(set(row)):
-            return False, f"Duplicate entries in row {i + 1}"
+            return False, f"Duplicate entries in row {i+1}"
 
         col = [board[j][i] for j in range(9) if board[j][i] != 0]
         if len(col) != len(set(col)):
-            return False, f"Duplicate entries in column {i + 1}"
+            return False, f"Duplicate entries in column {i+1}"
 
     # Check 3x3 boxes
     for box_row in range(3):
@@ -74,12 +70,10 @@ def check_for_duplicates(board):
 
     return True, ""
 
-
 def display_board(board):
     for i in range(9):
         for j in range(9):
-            entries[i][j].insert(0, board[i][j])
-
+                entries[i][j].insert(0,board[i][j])
 
 def get_board():
     board = []
@@ -91,23 +85,20 @@ def get_board():
         board.append(row)
     return board
 
-
 def solve():
     board = get_board()
     val, message = check_for_duplicates(board)
     if not val:
         messagebox.showerror("Error", message)
         return
-
+    
     solve_sudoku(board)
     display_board(board)
 
-
 def validate_input(M):
-    if M == "" or M in "123456789" and len(M) == 1:  # Allow empty input or digits 1-9
+    if M == "" or M in "123456789" and len(M)==1 :  # Allow empty input or digits 1-9
         return True
     return False
-
 
 root = tk.Tk()
 root.title("Sudoku Solver")
@@ -115,10 +106,10 @@ root.title("Sudoku Solver")
 entries = [[None for k in range(9)] for k in range(9)]
 for i in range(9):
     for j in range(9):
-        entry = tk.Entry(root, width=3, font=('Arial', 30), justify='center')
+        entry = tk.Entry(root,width=3, font=('Arial', 30), justify='center')
         entry.grid(row=i, column=j, padx=5, pady=5)
         entries[i][j] = entry
-        entry.config(validate="key", validatecommand=(root.register(validate_input), '%P'))
+        entry.config(validate="key",validatecommand = (root.register(validate_input), '%P'))
 
 solve_button = tk.Button(root, text="Solve", command=solve, font=('Arial', 16))
 solve_button.grid(row=9, columnspan=10, pady=10)
